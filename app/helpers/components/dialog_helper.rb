c46314c7e28aad0f371f31b07397b4cabd673d70
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2023-07-07 07:14:08 -0400

    Works with yields

--- app/helpers/components/dialog_helper.rb
+++ app/helpers/components/dialog_helper.rb
@@ -1,6 +1,11 @@
 module Components::DialogHelper
-  def render_dialog(trigger:, label:, description:, continue:, **options)
-    cancel = options[:cancel] || render_button("Cancel", class: "mt-2 sm:mt-0", data: {action: "ui--dialog#close"})
-    render "components/ui/dialog", trigger:, label:, description:, continue:, cancel:, **options
+  # def render_dialog(trigger:, label:, description:, continue:, **options)
+  #   cancel = options[:cancel] || render_button("Cancel", class: "mt-2 sm:mt-0", data: {action: "ui--dialog#close"})
+  #   render "components/ui/dialog", trigger:, label:, description:, continue:, cancel:, **options
+  # end
+
+  def render_dialog(**options, &block)
+    content = capture(&block) if block
+    render "components/ui/dialog", content: content, **options
   end
 end
