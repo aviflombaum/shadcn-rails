76e7a73aaafd30bcf26632011d1f5bb6f11b083a
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2023-07-12 04:36:37 -0400

    WIP

--- app/helpers/components/accordion_helper.rb
+++ app/helpers/components/accordion_helper.rb
@@ -1,5 +1,18 @@
 module Components::AccordionHelper
-  def render_accordion(title:, description:)
+  def accordion_title(&block)
+    content_for :title, capture(&block), flush: true
+  end
+
+  def accordion_description(&block)
+    content_for :description, capture(&block), flush: true
+  end
+
+  def render_accordion(title: nil, description: nil, &block)
+    if title
+      content_for :description, capture(&block), flush: true
+    elsif !title && !description
+      capture(&block)
+    end
     render "components/ui/accordion", title: title, description: description
   end
 end
