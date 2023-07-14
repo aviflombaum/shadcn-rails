4150fa027fa4b475dc9a4155f415eb3eaaf1870f
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2023-07-14 09:53:07 -0400

    Card documentation

--- app/helpers/components/card_helper.rb
+++ app/helpers/components/card_helper.rb
@@ -1,5 +1,5 @@
 module Components::CardHelper
-  def render_card(footer: nil, title: nil, subtitle: nil, body: nil, **options, &block)
+  def render_card(title: nil, subtitle: nil, body: nil, footer: nil, **options, &block)
     render "components/ui/card", title: title, subtitle: subtitle, footer: footer, body: (block ? capture(&block) : body), block:, options: options
   end
 end
