9c67bb03e8d5b5963c40570826d1b8d089630e99
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2023-07-14 01:50:28 -0400

    Button documentation

--- app/helpers/components/button_helper.rb
+++ app/helpers/components/button_helper.rb
@@ -1,5 +1,5 @@
 module Components::ButtonHelper
-  def render_button(label = "", data: "", text: "", variant: :default, as: :button, href: nil, **options)
+  def render_button(label = "", text: nil, variant: :default, as: :button, href: nil, data: {}, **options, &block)
     button_classes = " inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2 "
     varianet_classes = case variant.to_sym
     when :default
@@ -15,6 +15,7 @@ module Components::ButtonHelper
     end
     button_classes << " #{varianet_classes} #{options[:class]}"
     text = label if label.present?
+    text = capture(&block) if block
     render "components/ui/button", text:, button_classes:, as:, href:, data:, **options
   end
 end
