f986fa23ecdf73fb7fd2253ae98bfc5352052cd3
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2023-07-18 10:44:11 -0400

    Two more components to document

--- app/helpers/components/input_helper.rb
+++ app/helpers/components/input_helper.rb
@@ -1,7 +1,7 @@
 module Components::InputHelper
   def render_input(name:, label: false, id: nil, type: :text, value: nil, **options)
     options[:class] = "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 #{options[:class]} "
-    options[:class] << case options[:style]
+    options[:class] << case options[:variant]
     when :borderless
       " border-0 focus-visible:outline-none focus-visible:shadow-none focus-visible:ring-transparent"
     else
