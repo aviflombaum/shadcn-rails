943b0e11edb1951a2d832c62a5c354944acfbd26
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2023-07-25 17:54:41 -0400

    Fix input variant in filter

--- app/helpers/components/input_helper.rb
+++ app/helpers/components/input_helper.rb
@@ -1,14 +1,16 @@
 module Components::InputHelper
   def render_input(name:, label: false, id: nil, type: :text, value: nil, **options)
-    options[:class] = "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm  shadow-sm transition-colors ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 #{options[:class]} "
+    options[:class] = "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm transition-colors ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 #{options[:class]} "
     options[:class] << case options[:variant]
     when :borderless
       " border-0 focus-visible:outline-none focus-visible:shadow-none focus-visible:ring-transparent"
     else
-      " focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:border-muted"
+      "shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:border-muted"
     end
+    options[:class] = tw(options[:class])
+
     options.reverse_merge!(
-      label: false,
+      label: (options[:lable] || false),
       required: (options[:required] || false),
       disabled: (options[:disabled] || false),
       readonly: (options[:readonly] || false),
