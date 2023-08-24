03468126b7e21bf9f2500b59498efdc471730c14
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2023-08-24 08:23:20 -0400

    Table documentation

--- app/helpers/components/table_helper.rb
+++ app/helpers/components/table_helper.rb
@@ -1,6 +1,8 @@
 module Components::TableHelper
-  def render_table(caption = nil, &block)
-    content_tag :table, class: "w-full text-sm border-b" do
+  def render_table(caption = nil, **options, &block)
+    content_tag :table, options.reverse_merge(
+      class: tw("w-full text-sm border-b", options[:class])
+    ) do
       if caption.present?
         content_tag :caption, caption, class: "mt-4 text-sm text-muted-foreground " do
           capture(&block)
@@ -11,8 +13,10 @@ module Components::TableHelper
     end
   end
 
-  def table_head(&block)
-    content_tag :thead, class: "[&_tr]:border-b" do
+  def table_head(**options, &block)
+    content_tag :thead, options.reverse_merge(
+      class: tw("[&_tr]:border-b", options[:class])
+    ) do
       content_tag :tr, class: "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted" do
         capture(&block)
       end
@@ -20,7 +24,9 @@ module Components::TableHelper
   end
 
   def table_header(content = nil, **options, &block)
-    content_tag :th, class: tw("h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0", options[:class]) do
+    content_tag :th, options.reverse_merge(
+      class: tw("h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0", options[:class])
+    ) do
       if block
         capture(&block)
       else
@@ -29,20 +35,22 @@ module Components::TableHelper
     end
   end
 
-  def table_body(&block)
-    content_tag :tbody, class: "[&_tr:last-child]:border-0", &block
+  def table_body(**options, &block)
+    content_tag :tbody, class: options.reverse_merge(
+      class: tw("[&_tr:last-child]:border-0", options[:class])
+    ), &block
   end
 
   def table_row(**options, &block)
-    content_tag :tr,
-      options.reverse_merge(
-        class: tw("border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted", options[:class])
-      ),
-      & block
+    content_tag :tr, options.reverse_merge(
+      class: tw("border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted", options[:class])
+    ), &block
   end
 
-  def table_column(content = nil, &block)
-    content_tag :td, class: "p-4 align-middle [&:has([role=checkbox])]:pr-0 font-medium" do
+  def table_column(content = nil, **options, &block)
+    content_tag :td, options.reverse_merge(
+      class: tw("p-4 align-middle [&:has([role=checkbox])]:pr-0 font-medium", options[:class])
+    ) do
       if block
         capture(&block)
       else
