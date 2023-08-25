80e7bd3729b5056474db4e204e1e3338b8e4685e
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2023-08-25 06:22:30 -0400

    Alert Dialog generator spec

--- spec/generators/alert_dialog_generator_spec.rb
+++ spec/generators/alert_dialog_generator_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 require_relative "../../lib/generators/shadcn-ui_generator"
 
 RSpec.describe ShadcnUiGenerator, type: :generator do
-  let(:component_name) { "dialog" }
+  let(:component_name) { "alert-dialog" }
   let(:rails_root) { "#{Rails.root}/spec/dummy_app/tmp" }
 
   before(:each) do
@@ -22,12 +22,14 @@ RSpec.describe ShadcnUiGenerator, type: :generator do
   it "copies the component files to the correct destination" do
     described_class.start([component_name, rails_root])
 
-    expect(File).to exist("#{rails_root}/app/views/components/ui/_#{component_name}.html.erb")
-    expect(File).to exist("#{rails_root}/app/javascript/controllers/ui/#{component_name}_controller.js")
-    expect(File).to exist("#{rails_root}/app/helpers/components/#{component_name}_helper.rb")
+    expect(File).to exist("#{rails_root}/app/views/components/ui/_alert_dialog.html.erb")
+    expect(File).to exist("#{rails_root}/app/helpers/components/alert_dialog_helper.rb")
   end
 
   it "copies the dependency files to the correct destination" do
     expect(File).to exist("#{rails_root}/app/views/components/ui/shared/_backdrop.html.erb")
+    expect(File).to exist("#{rails_root}/app/helpers/components/dialog_helper.rb")
+    expect(File).to exist("#{rails_root}/app/views/components/ui/_dialog.html.erb")
+    expect(File).to exist("#{rails_root}/app/javascript/controllers/ui/dialog_controller.js")
   end
 end
