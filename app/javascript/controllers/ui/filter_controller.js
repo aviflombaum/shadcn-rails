eba940fb694e6964186aa58a16a79cf753665901
jmsanchezdiaz <jmsanchezdiaz02@gmail.com>
2024-12-27 17:47:58 -0300

    Add modes to filter + documentation and new approach to filtering.

--- app/javascript/controllers/ui/filter_controller.js
+++ app/javascript/controllers/ui/filter_controller.js
@@ -2,18 +2,23 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class UIFilter extends Controller {
   static targets = ["source", "item"];
+  static values = {
+    mode: String,
+  };
 
   connect() {}
 
   filter(event) {
     let lowerCaseFilterTerm = this.sourceTarget.value.toLowerCase();
-    let regex = new RegExp("^" + lowerCaseFilterTerm);
     if (this.hasItemTarget) {
       this.itemTargets.forEach((el, i) => {
         let filterableKey = el.innerText.toLowerCase();
-
+        const shouldToggle =
+          this.modeValue === "includes"
+            ? !filterableKey.trim().includes(lowerCaseFilterTerm)
+            : !filterableKey.trim().startsWith(lowerCaseFilterTerm);
         // Check for consecutive characters match using regex
-        el.classList.toggle("hidden", !regex.test(filterableKey));
+        el.classList.toggle("hidden", shouldToggle);
       });
     }
   }
