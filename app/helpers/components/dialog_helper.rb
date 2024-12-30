b6c992205117c24d6bc6c6c5a8c4b07618f6b6b1
Lucas Belenda <lucas.belenda@sinaptia.dev>
2024-12-30 15:42:25 -0300

    allow opening the dialog by default trough the state parameter

--- app/helpers/components/dialog_helper.rb
+++ app/helpers/components/dialog_helper.rb
@@ -1,11 +1,12 @@
 module Components::DialogHelper
   def render_dialog(**options, &block)
     content = capture(&block) if block
+    options[:state] = options[:state] || "closed"
     render "components/ui/dialog", content: content, options: options
   end
 
-  def dialog_trigger(&block)
-    content_for :dialog_trigger, capture(&block), flush: true
+  def dialog_trigger(**options, &block)
+    content_for :dialog_trigger, capture(&block), options: options, flush: true
   end
 
   def dialog_content(&block)
